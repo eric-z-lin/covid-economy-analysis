e9580da474e8acc0c521e0144d3db6c6fd1a86c0
Eric Lin <eric_lin@college.harvard.edu>
2020-12-06 21:38:16 -0500

    Final submission

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # covid-economy-analysis
 Harvard CS 171 Final Project
 
+Website hosted on: 
+
+Developed by Eric Lin, Sophie Sun, and Verena Lin.
+
 Files:
 
 1. README.md: summarizes all of the files relating to this final project and their contents
@@ -9,26 +13,28 @@ Files:
 
 3. CS171_Final_Project.pdf: contains all of the tasks completed from Weeks 8-14 (including our Process Book, Project Proposal, Team Agreement, Detailed Project Plan, Data, Sketches, Storyboard, Prototype V1, Prototype V2, and Think Aloud Study Results).
 
-4. v2 folder:
-	a. css folder:
-		i. boostrap,css, bootstrap.css.map, bootstrap.min.css: css files from the online repository.
-		ii. style.css: contains our groups' styling choices.
-	b. data folder:
-		i. census_usa.csv: census data from each of the 50 states from 2010 to 2019.
-		ii. gdp_clean.csv: monthly US GDP (in $T) from Jun 2019 - Sep 2020.
-		iii. mrtssales92-present.xls: monthly sales (in $M) of industries from Jan 1992 - Aug 2020.
-		iv. sales.csv: cleaned version of mrtssales92-present.xls.
-		v. states-10m.json: data to create a US map from the online repository.
-		vi.us_map_grid.csv: lockdown status of states due to COVID-19.
-		vii.words.tsv: dummy data.
-		viii. ppp_data.csv: PPP given to every state from Apr 2020 - Aug 2020. 
-	c. index.html: displays the text on the left hand side during scroll, see js folder for implementation of 	    visualizations.
-	d. js folder: 
-		i. helpers.js: stores names of states and their abbreviations.
-		ii. mapVis.js: contains the map visualization of PPP data (on hover, it displays PPP information 		    by state).
-		iii. scroller.js: contains the position of the scroll in order to decide which visualizations to 			     display and which visualizations to hide in the html file as the user scrolls.
-		iv. sections.js: contains the code for initializing the visualizations, wrangling the data, and 			     updating the visualizations, as well as ensuring that the correct visualizations are displayed 		     and hidden at the correct locations (help from http://bost.ocks.org/mike/chart/).
-	e. lib folder: 
-		i. d3.js: helper libraries from the online repository.
-		ii. d3.min.js: helper libraries from the online repository.
+4. final_code folder:
+    - css folder:
+        - boostrap,css, bootstrap.css.map, bootstrap.min.css: css files from the online repository.
+        - style.css: contains our groups' styling choices.
+    - data folder:
+        - census_usa.csv: census data from each of the 50 states from 2010 to 2019.
+        - gdp_clean.csv: monthly US GDP (in $T) from Jun 2019 - Sep 2020.
+        - mrtssales92-present.xls: monthly sales (in $M) of industries from Jan 1992 - Aug 2020.
+        - sales.csv: cleaned version of mrtssales92-present.xls.
+        - states-10m.json: data to create a US map from the online repository.
+        - us_map_grid.csv: lockdown status of states due to COVID-19.
+        - words.tsv: dummy data.
+        - ppp_data.csv: PPP given to every state from Apr 2020 - Aug 2020. 
+    - index.html: displays the text on the left hand side during scroll, see js folder for implementation of 	    visualizations.
+    - js folder: 
+        - helpers.js: stores names of states and their abbreviations.
+        - mapVis.js: contains the map visualization of PPP data (on hover, it displays PPP information 		    by state).
+        - scroller.js: contains the position of the scroll in order to decide which visualizations to 			     display and which visualizations to hide in the html file as the user scrolls.
+        - sections.js: contains the code for initializing the visualizations, wrangling the data, and 			     updating the visualizations, as well as ensuring that the correct visualizations are displayed 		     and hidden at the correct locations (help from http://bost.ocks.org/mike/chart/).
+        - draw_script.js: contains the code for drawing consumer expenditure
+    - lib folder: 
+        - d3.js: helper libraries from the online repository.
+        - d3.min.js: helper libraries from the online repository.
 	
+5. cs171_video.mp4: Our 2-minute video walkthrough of our website.
---
+++ final_code/js/mapVis.js
@@ -0,0 +1,258 @@
+/* * * * * * * * * * * * * *
+*          MapVis          *
+* * * * * * * * * * * * * */
+
+
+class MapVis {
+
+    // constructor method to initialize Map object
+    constructor(parentElement, geoData, usaData, pppData, selectedCategory) {
+        this.parentElement = parentElement;
+        this.geoData = geoData;
+        this.usaData = usaData;
+        this.pppData = pppData;
+        this.selectedCategory = selectedCategory;
+        this.displayData = [];
+
+        // parse date method
+        this.parseDate = d3.timeParse("%m/%d/%Y");
+
+        this.colors = d3.scaleLinear()
+            .range(['#FFFFFF', '#EAA0A1']);
+
+        this.initMap()
+    }
+
+    initMap() {
+        let mapObject = this
+
+        mapObject.margin = {top: 20, right: 100, bottom: 100, left: 100};
+        mapObject.width = $("#" + mapObject.parentElement).width() - mapObject.margin.left - mapObject.margin.right;
+        mapObject.height = $("#" + mapObject.parentElement).height() - mapObject.margin.top - mapObject.margin.bottom;
+
+        // init drawing area
+        mapObject.svg = d3.select("#" + mapObject.parentElement).append("svg")
+            .attr("width", mapObject.width)
+            .attr("height", mapObject.height)
+            .attr('transform', `translate (${mapObject.margin.left}, ${mapObject.margin.top})`);
+
+        mapObject.path = d3.geoPath();
+        mapObject.us = mapObject.geoData;
+
+        mapObject.viewpoint = {'width': 975, 'height': 610};
+        mapObject.zoom = mapObject.width / mapObject.viewpoint.width;
+
+        // adjust map position
+        mapObject.map = mapObject.svg.append("g") // group will contain all state paths
+            .attr("class", "states")
+            .attr('transform', `scale(${mapObject.zoom} ${mapObject.zoom})`);
+
+        mapObject.states = mapObject.map.selectAll(".state")
+            .data(topojson.feature(mapObject.us, mapObject.us.objects.states).features)
+            .enter().append("path")
+            .attr('class','state')
+            .attr("d", mapObject.path);
+        mapObject.map.append("path")
+            .datum(topojson.mesh(mapObject.us, mapObject.us.objects.states, (a, b) => a !== b))
+            .attr("fill", "none")
+            .attr("stroke", "#627D98")
+            .attr("stroke-linejoin", "round")
+            .attr("pointer-events", "none")
+            .attr("d", mapObject.path);
+
+        // append tooltip
+        mapObject.tooltip = d3.select("body").append('div')
+            .attr('class', "tooltip")
+            .attr('id', 'mapTooltip');
+
+        // append legend
+        mapObject.legend = mapObject.svg.append("rect")
+            .attr('transform', `translate(${mapObject.width * 2.8 / 4.5}, ${mapObject.height - 40})`)
+            .attr("width", 200)
+            .attr("height", 20);
+        mapObject.defs = mapObject.svg.append("defs");
+        mapObject.linearGradient = mapObject.defs.append("linearGradient")
+            .attr("id", "linear-gradient")
+            .attr("x1", "0%")
+            .attr("y1", "0%")
+            .attr("x2", "100%")
+            .attr("y2", "0%");
+
+        // append legend's axis
+        mapObject.legendScale = d3.scaleLinear()
+            .range([0,200])
+        mapObject.legendAxis = d3.axisBottom()
+            .scale(mapObject.legendScale)
+            .ticks(3);
+        mapObject.axis = mapObject.svg.append("g")
+            .attr("class", "x-axis axis")
+            .attr('transform', `translate(${mapObject.width * 2.8 / 4.5}, ${mapObject.height - 20})`)
+
+        mapObject.wrangleData()
+
+    }
+
+    wrangleData() {
+        let mapObject = this;
+
+        // first, filter according to selectedTimeRange, init empty array
+        let filteredData = [];
+
+
+        filteredData = mapObject.pppData;
+
+        // prepare covid data by grouping all rows by state
+        let pppDataByState = Array.from(d3.group(filteredData, d => d.State), ([key, value]) => ({key, value}))
+
+        // init final data structure in which both data sets will be merged into
+        mapObject.stateInfo = []
+
+        // merge
+        pppDataByState.forEach( state => {
+
+            // get full state name
+            let stateName = nameConverter.getFullName(state.key)
+
+            // init counters
+            let population = 0;
+            let aCount = 0;
+            let bCount = 0;
+            let cCount = 0;
+            let dCount = 0;
+            let eCount = 0;
+
+            // look up population for the state in the census data set
+            mapObject.usaData.forEach( row => {
+                if(row.state === stateName){
+                    population += +row["2019"].replaceAll(',', '');
+                }
+            })
+
+            // calculate new cases by summing up all the entries for each state
+            state.value.forEach( entry => {
+                if (entry.LoanRange === "a $5-10 million") {
+                    aCount++;
+                }
+                else if (entry.LoanRange === "b $2-5 million") {
+                    bCount++;
+                }
+                else if (entry.LoanRange === "c $1-2 million") {
+                    cCount++;
+                }
+                else if (entry.LoanRange === "d $350,000-1 million") {
+                    dCount++;
+                }
+                else {
+                    eCount++;
+                }
+            });
+
+            let loanCount = aCount + bCount + cCount + dCount + eCount;
+
+            // populate the final data structure
+            if (stateName === "" | stateName === "American Samoa" | stateName === "Guam" | stateName === "Puerto Rico" | stateName === "US Virgin Islands"){
+                return null;
+            }
+            else {
+                mapObject.stateInfo.push(
+                    {
+                        state: stateName,
+                        population: population,
+                        loanCount: loanCount,
+                        relLoanCount: (loanCount/population*100),
+                        a: aCount,
+                        b: bCount,
+                        c: cCount,
+                        d: dCount,
+                        e: eCount
+                    }
+                )
+            }
+
+        });
+
+        console.log('final data structure for myMapVis', mapObject.stateInfo);
+
+        mapObject.updateMap();
+    }
+
+    findValue(stateName) {
+        let mapObject = this;
+        let returnValue = [];
+        mapObject.stateInfo.forEach( state => {
+            if (stateName === state.state) {
+                returnValue = state;
+            }
+        })
+        return returnValue;
+    }
+
+    updateMap() {
+        let mapObject = this;
+
+        // update color domain
+        mapObject.colors.domain([0,
+            d3.max(mapObject.stateInfo, function(d){
+                return d[mapObject.selectedCategory];
+            })
+        ]);
+
+        mapObject.states.style("fill", function(d, index) {
+                let thisStatesInfo = mapObject.findValue (d.properties.name);
+                console.log(thisStatesInfo)
+                return mapObject.colors(thisStatesInfo[mapObject.selectedCategory]);
+            })
+            .on('mouseover', function(event, d){
+                let thisStatesInfo = mapObject.findValue (d.properties.name);
+
+                d3.select(this)
+                    .attr('stroke-width', '2px')
+                    .attr('stroke', 'black');
+
+                mapObject.tooltip
+                    .style("opacity", 1)
+                    .style("left", event.pageX + 20 + "px")
+                    .style("top", event.pageY + "px")
+                    .html(`
+                <div style="border: thin solid grey; border-radius: 5px; background: #627D98; padding: 20px">
+                     <h3> ${thisStatesInfo.state}<h3>
+                     <h4> Population: ${thisStatesInfo.population}</h4>
+                     <h4> Loans (absolute): ${thisStatesInfo.loanCount}</h4>
+                     <h4> Loans (relative per capita): ${thisStatesInfo.relLoanCount}</h4>
+                     <h4> $5-10 million: ${thisStatesInfo.a}</h4>  
+                     <h4> $2-5 million: ${thisStatesInfo.b}</h4>
+                     <h4> $1-2 million: ${thisStatesInfo.c}</h4>
+                     <h4> $350,000-1 million: ${thisStatesInfo.d}</h4>
+                     <h4> $150,000-350,000: ${thisStatesInfo.e}</h4>
+                </div>`);
+            })
+            .on('mouseout', function(event, d){
+                d3.select(this)
+                    .attr('stroke-width', '0px')
+
+                mapObject.tooltip
+                    .style("opacity", 0)
+                    .style("left", 0)
+                    .style("top", 0)
+                    .html(``);
+            });
+
+        // draw legend axis
+        mapObject.legendScale.domain([0,
+            d3.max(mapObject.stateInfo, function(d){
+                return d[mapObject.selectedCategory];
+            })
+        ]);
+        mapObject.svg.select(".x-axis").call(mapObject.legendAxis);
+
+        // append legend
+        mapObject.linearGradient.append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", '#FFFFFF');
+        mapObject.linearGradient.append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", '#EAA0A1');
+        mapObject.legend.style("fill", "url(#linear-gradient)");
+
+    }
+}
